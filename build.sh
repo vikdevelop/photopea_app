ae66208782ba4ab7dd9119963d137fde99ec7e1a
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2021-11-03 14:50:19 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #/bin/sh
 
-c++ ./photopea/photopea.cpp `pkg-config --cflags --libs gtk+-3.0 webkit2gtk-4.0` -o photopea-app
-install -Dm755 -t /app/bin photopea_app
+mkdir build_photopea
+cd build_photopea
+c++ photopea.cpp `pkg-config --cflags --libs gtk+-3.0 webkit2gtk-4.0` -o photopea-app
+install -Dm755 -t /app/bin build_photopea/photopea_app
