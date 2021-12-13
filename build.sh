0891d8865b2f7846146058e5ad06f34fce00ea3f
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2021-12-13 16:41:00 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
-#/bin/sh
+#!/bin/sh
 
-mkdir build_photopea
-cd build_photopea
-c++ ./photopea.cpp `pkg-config --cflags --libs gtk+-3.0 webkit2gtk-4.0` -o photopea_app
-install -Dm755 -t /app/bin ./build_photopea/photopea_app
+# for Flatpak:
+mkdir ./build
+cd ./build
+c++ photopea.cpp `pkg-config --cflags --libs gtk+-3.0 webkit2gtk-4.0` -o photopea_app
+install -Dm755 -t /app/bin ./photopea_app
+
+# for all Linux distros (uncoonment lines below):
+# mkdir ./build
+# cd ./build
+# c++ photopea.cpp `pkg-config --cflags --libs gtk+-3.0 webkit2gtk-4.0` -o photopea_app
+# install -Dm755 ./photopea_app /usr/bin/photopea_app
