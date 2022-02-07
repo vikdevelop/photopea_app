a3b9dde170950a160c99f92fdd010abc3317cd10
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-02-07 21:50:13 +0100

    Create install.py

---
+++ install.py
@@ -0,0 +1,16 @@
+import os
+import shutil
+os.system("npm install")
+os.system("sudo -u")
+os.makedirs("/usr/share/photopea_app")
+shutil.move("node_modules", "/usr/share/photopea_app/")
+shutil.move("com.github.vikdevelop.photopea_app.desktop", "/usr/share/applications/")
+shutil.move("data/icons/com.github.vikdevelop.photopea_app.png", "/usr/share/icons/hicolor/128x128/apps/")
+shutil.move("src", "/usr/share/photopea_app/")
+shutil.move("package.json", "/usr/share/photopea_app/")
+shutil.move("package-lock.json", "/usr/share/photopea_app/")
+os.makedirs("/usr/share/licenses/photopea_app")
+shutil.move("LICENSE", "/usr/share/licenses/photopea_app/")
+shutil.move("data/com.github.vikdevelop.photopea_app.metainfo.xml", "/usr/share/metainfo/")
+shutil.move("run.py", "/usr/bin/")
+print("Photopea was installed successfully!")
