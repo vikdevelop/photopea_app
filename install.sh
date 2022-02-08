1e0e3e36224ba38bcee28b911225588f46ceada4
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-02-08 12:35:56 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 # Install desktop file, icon, appdata file & LICENSE
-cp -r com.github.vikdevlop.photopea_app.desktop /usr/share/applications/
-cp -r data/com.github.vikdevlop.photopea_app.metainfo.xml /usr/share/metainfo/
-cp -r data/icons/com.github.vikdevlop.photopea_app.png /usr/share/icons/hicolor/128x128/apps/
-cp -r LICENSE /usr/share/licenses/photopea_app/
+cp com.github.vikdevlop.photopea_app.desktop /usr/share/applications/
+cp data/com.github.vikdevlop.photopea_app.metainfo.xml /usr/share/metainfo/
+cp data/icons/com.github.vikdevlop.photopea_app.png /usr/share/icons/hicolor/128x128/apps/
+cp LICENSE /usr/share/licenses/photopea_app/
 
 # Install Binary file
 cp -R dist/photopea-linux-x64 /usr/share/
