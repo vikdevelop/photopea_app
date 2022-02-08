c051b2860ac806f02286362311373e9a186e4608
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-02-08 12:33:24 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/bash
 # Install desktop file, icon, appdata file & LICENSE
-sudo install -D -t com.github.vikdevlop.photopea_app.desktop /usr/share/applications
-sudo install -D -t data/com.github.vikdevlop.photopea_app.metainfo.xml /usr/share/metainfo
-sudo install -D -t data/icons/com.github.vikdevlop.photopea_app.png /usr/share/icons/hicolor/128x128/apps
-sudo install -D -t LICENSE /usr/share/licenses/photopea_app
+cp -r com.github.vikdevlop.photopea_app.desktop /usr/share/applications/
+cp -r data/com.github.vikdevlop.photopea_app.metainfo.xml /usr/share/metainfo/
+cp -r data/icons/com.github.vikdevlop.photopea_app.png /usr/share/icons/hicolor/128x128/apps/
+cp -r LICENSE /usr/share/licenses/photopea_app/
 
-# Build
-sudo cp -R dist/photopea-linux-x64 /usr/share/
-cd /usr/share/dist/photopea-linux-x64 
-sudo ln -s photopea photopea_bin
-sudo install -D -t photopea_bin /usr/bin
+# Install Binary file
+cp -R dist/photopea-linux-x64 /usr/share/
+cd /usr/share/photopea-linux-x64 
+ln -s photopea photopea_bin
+cp photopea_bin /usr/bin/
+echo "Photopea was installed successfully!"
