7b4b1f9679d4647f05df46072aaa2008aa7523aa
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2024-03-18 20:15:31 +0100

    add adblock

--- src/main/main.js
+++ src/main/main.js
@@ -1,34 +1,35 @@
-const {app, BrowserWindow} = require('electron')
-const { blockWindowAds } = require('@cliqz/adblocker-electron')
-const path = require('path')
+const { app, BrowserWindow } = require('electron');
+const { blockWindowAds } = require('@cliqz/adblocker-electron');
+const path = require('path');
 
-function createWindow () {
+async function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 3840,
     height: 2160,
     icon: 'data/icons/com.github.vikdevelop.photopea_app.png',
     title: "Photopea",
-    autoHideMenuBar: 'true',
-    transparent: 'true',
+    autoHideMenuBar: true,
+    transparent: true,
     webPreferences: {
       preload: path.join(__dirname, 'src/renderer/preload.js')
     }
-  })
-  // load Photopea webapp
-  mainWindow.loadURL('https://photopea.com')
-  blockWindowAds(mainWindow);
-  
+  });
+
+  // Load Photopea webapp
+  mainWindow.loadURL('https://photopea.com');
+
+  // Block ads
+  await blockWindowAds(mainWindow);
 }
 
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
 
   app.on('activate', function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
-
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
+  if (process.platform !== 'darwin') app.quit();
+});
