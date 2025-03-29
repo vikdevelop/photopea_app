31a0c6a9ff07dc25c07d041cdbf9ca8b71ab734a
vikdevelop <vikdevelop@proton.me>
2025-03-29 21:03:27 +0100

    Remove with the side panel with ads
    based on #7

---
+++ src/main.js
@@ -0,0 +1,47 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+const devMode = false;
+
+async function createWindow() {
+    const win = new BrowserWindow({
+        width: 1920,
+        height: 1080,
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: true,
+            preload: path.join(__dirname, 'renderer/renderer.js')
+        },
+    });
+
+    if (devMode) {
+        win.setMenuBarVisibility(true);
+        win.webContents.openDevTools();
+    } else {
+        win.setMenuBarVisibility(false);
+    }
+
+    win.on('close', () => {
+        win.destroy();
+    });
+
+    console.log('Loading Photopea with #8887');
+    await win.loadURL('https://www.photopea.com/#8887');
+
+}
+
+app.whenReady().then(() => {
+    createWindow().catch(err => console.error('App failed:', err.message));
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow().catch(err => console.error('Reactivation failed:', err.message));
+    }
+});
